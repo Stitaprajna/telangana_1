d2f6437f9fbb94c4c6ae0ae5d8739f73a603639e
Stitaprajna Panda <stitaprajna.panda@gmail.com>
2022-12-31 12:07:16 +0530

    Added

--- fire_telangana.py
+++ fire_telangana.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-
+with st.sidebar:
+  st.image('images (1).jpeg')
+  st.title('Agricultural Fire causes Global Warming')
 st.title('Locations of Agriculural Fire 🔥 in Telangana')
 
 #@st.cache(persist=True)
 df = pd.read_csv('task_ff.csv')
 df = df.drop(['Unnamed: 0'],axis=1)
 
-v = df[df['cluster']==2]
+v = df[df['cluster']==0]
 l1 = list(v['District'].value_counts().index)
 l2 = []
 l3 = []
@@ -23,12 +25,17 @@ vv['longitude'] = pd.DataFrame(l3)
 vv['No_of_fire_events'] = pd.DataFrame(v['District'].value_counts().values)
 
 st.header('Which District has the most number of Agricultural Fire')
-agri_fire_events = st.slider('No. of Agricultural fires in Telangana',2,534)
+agri_fire_events = st.slider('No. of Agricultural fires in Telangana',0,1500)
 st.map(vv.query('No_of_fire_events >= @agri_fire_events')[['latitude','longitude']])
+
+#st.header('Districtwise location of fire events')
+
+#st.map(vv.query('No_of_fire_events >= @agri_fire_events')[['latitude','longitude']])
+
 
 if st.checkbox('Show raw data',False):
     st.subheader('Raw Data')
-    st.write(vv)
+    st.write(df)
 
 
 
